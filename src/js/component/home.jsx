63d8f1ad87a58e92fb1e86c9307915a2ddb0bc79
angel2388 <rafa2388@gmail.com>
2022-10-11 16:23:31 +0000

    Traffic Light with React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ encendido, setIluminado] = useState ("");
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="semaforo">
+			<div
+			onClick={() => setIluminado("rojo")}
+			id="rojoluz"
+			 className={ encendido == "rojo" ? "sombra" : "no"}>
+			 </div>
+			
+			<div 
+			onClick={() => setIluminado("amarillo")}
+			id="amarilloluz"
+			className={encendido == "amarillo" ? "sombra" : "no"}>
+			</div>
+			
+			<div 
+			onClick={() => setIluminado("verde")}
+			id="verdeluz"
+			className={ encendido == "verde" ? "sombra" : "no"}>
+			</div>
+		
 		</div>
 	);
 };
